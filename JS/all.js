f9b5e8e94255373dfd8e09468d3f5484cd62a2af
Murilo_Muller <murilomuller@protonmail.com>
2022-10-16 11:18:42 -0300

    anotações sobre function

---
+++ JS/all.js
@@ -0,0 +1,18 @@
+// aulas anteriores não foram feitas
+
+// Aula sobre Function 02
+const carro = {
+  marca: 'Ford',
+  ano: 2018,
+  acelerar: function (velocidade, tempo) {
+    return `${this.marca} acelerou ${velocidade}km/h em ${tempo}`
+  }
+}
+
+const fiat = {
+  marca: 'Fiat'
+}
+
+const velocidadeFiat = carro.acelerar.bind(fiat)
+
+console.log(acelerarHonda(300, 20))
