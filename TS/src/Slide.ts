87d3c4738ef06a1f075d2f5aa255e720f2932c96
Murilo Muller <murilomuller@protonmail.com>
2022-11-23 14:12:19 -0300

    TS-files finished

--- TS/src/Slide.ts
+++ TS/src/Slide.ts
@@ -87,6 +87,7 @@ export default class Slide {
     this.show(next)
   }
   pause() {
+    document.body.classList.add('paused')
     this.pausedTimeout = new Timeout(() => {
       this.timeout?.pause()
       this.paused = true
@@ -95,6 +96,7 @@ export default class Slide {
     }, 300)
   }
   continue() {
+    document.body.classList.remove('paused')
     this.pausedTimeout?.clear()
     if (this.paused) {
       this.paused = false
@@ -112,7 +114,8 @@ export default class Slide {
     this.controls.appendChild(nextButton)
 
     this.controls.addEventListener('pointerdown', () => this.pause())
-    this.controls.addEventListener('pointerup', () => this.continue())
+    document.addEventListener('pointerup', () => this.continue())
+    document.addEventListener('touchend', () => this.continue())
     prevButton.addEventListener('pointerup', () => this.prev())
     nextButton.addEventListener('pointerup', () => this.next())
   }
