a64580388bc084950d31dff409010745bf42ac09
Murilo Muller <murilomuller@protonmail.com>
2022-11-23 14:12:32 -0300

    dist finished

--- TS/dist/Slide.js
+++ TS/dist/Slide.js
@@ -81,6 +81,7 @@ export default class Slide {
         this.show(next);
     }
     pause() {
+        document.body.classList.add('paused');
         this.pausedTimeout = new Timeout(() => {
             this.timeout?.pause();
             this.paused = true;
@@ -90,6 +91,7 @@ export default class Slide {
         }, 300);
     }
     continue() {
+        document.body.classList.remove('paused');
         this.pausedTimeout?.clear();
         if (this.paused) {
             this.paused = false;
@@ -107,7 +109,8 @@ export default class Slide {
         this.controls.appendChild(prevButton);
         this.controls.appendChild(nextButton);
         this.controls.addEventListener('pointerdown', () => this.pause());
-        this.controls.addEventListener('pointerup', () => this.continue());
+        document.addEventListener('pointerup', () => this.continue());
+        document.addEventListener('touchend', () => this.continue());
         prevButton.addEventListener('pointerup', () => this.prev());
         nextButton.addEventListener('pointerup', () => this.next());
     }
